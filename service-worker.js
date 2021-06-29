9e0a040255ce5b405e19cb3492aa680b67ee16fc
chaoyidaozhan <huangxiaogang123@aliyun.com>
2021-06-29 21:35:17 +0800

    第一次测试Service Worker

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,24 @@ self.addEventListener('install', event => {
     event.waitUtil(
         caches.open(cacheName)
         .then(cache => {
+            //缓存
             cache.addAll([
                 './sw.js',
                 './images/unicorn.jpg'
             ])
+        })
+    )
+})
+
+//读取缓存
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(res => {
+            if(response) {
+                return response;
+            }
+            return fetch(event.request);
         })
     )
 })
