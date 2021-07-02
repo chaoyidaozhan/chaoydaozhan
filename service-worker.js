72f0fd1212c17352a9aba9e6c9a8480230f48b38
huangxiaogang <huangxiaogang@baijia.com>
2021-07-02 11:18:18 +0800

    修复问题

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,29 @@
 var cacheName = 'helloWorld'
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(cacheName)
-        .then(cache => {
-            //缓存
-            cache.addAll([
-                './sw.js',
-                './images/unicorn.jpg'
-            ])
-        })
-    )
-})
+// self.addEventListener('install', event => {
+//     event.waitUntil(
+//         caches.open(cacheName)
+//         .then(cache => {
+//             //缓存
+//             cache.addAll([
+//                 './sw.js',
+//                 './images/unicorn.jpg'
+//             ])
+//         })
+//     )
+// })
 
-//读取缓存
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(res => {
-            if(res) {
-                return res;
-            }
-            return fetch(event.request);
-        })
-    )
-})
+// //读取缓存
+// self.addEventListener('fetch', event => {
+//     event.respondWith(
+//         caches.match(event.request)
+//         .then(res => {
+//             if(res) {
+//                 return res;
+//             }
+//             return fetch(event.request);
+//         })
+//     )
+// })
 
 self.addEventListener('fetch', e => {
     e.respondWith(
