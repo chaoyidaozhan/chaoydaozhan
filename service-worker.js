95db29bc8e677e2a1ef082f68fe26db83f857dc7
huangxiaogang <huangxiaogang@baijia.com>
2021-07-07 20:30:12 +0800

    获取webp图片

--- service-worker.js
+++ service-worker.js
@@ -18,7 +18,7 @@ self.addEventListener('fetch', e => {
         if(supportWebp) {
             var req = e.request.clone();
             var returnUrl = req.url.substr(0, req.url.lastIndexOf('.') + '.webp');
-            e.responseWith(
+            e.respondWith(
                 fetch(returnUrl, {
                     mode: 'no-cors'
                 })
